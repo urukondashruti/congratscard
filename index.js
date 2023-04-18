718c7772d5ab1c42a84801f2b1ab9d773e5bac68
shruti <urukondashruti1@gmail.com>
2023-04-18 09:10:38 +0000

    congratscard

---
+++ index.js
@@ -0,0 +1,19 @@
+const element = (
+  <div class="back">
+    <h1>Congratulations</h1>
+    <div class="con">
+      <img
+        className="img"
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png"
+      />
+      <h1>Kiran V</h1>
+      <p>Vishnu Institute of Computer Education and Technology,Bhimavaram</p>
+      <img
+        class="img1"
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
